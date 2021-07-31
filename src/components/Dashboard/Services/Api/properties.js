962f771c84c8aa7b73177f310f2f00d96fc6d4b7
Mateus de Oliveira <mateusdeoliveira1530@gmail.com>
2021-07-30 22:49:08 -0300

    Adicionando variaveis de ambiente

--- src/components/Dashboard/Services/Api/properties.js
+++ src/components/Dashboard/Services/Api/properties.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const propertiesApi = axios.create({
-  baseURL: 'http://localhost:3000/api/properties',
+  baseURL: `${process.env.API_URL}/api/properties`,
 })
 
 export default propertiesApi
