f7090dce7d72f847dbf3263afa8563245093bbf8
Mateus de Oliveira <mateusdeoliveira1530@gmail.com>
2021-07-30 22:15:21 -0300

    Resolvendo bug de camelCase que impediram o deploy no vercel

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,11 +8,7 @@ export default class Document extends NextDocument {
       <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossorigin="anonymous"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap"
             rel="stylesheet"
