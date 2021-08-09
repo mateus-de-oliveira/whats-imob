aedb6e7da310a3690d91bd19f8ebc24ddee193c9
Mateus de Oliveira <mateusdeoliveira1530@gmail.com>
2021-08-09 07:06:00 -0300

    removendo lib open-automate-wa para resolver erro de deploy

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import { Flex } from '@chakra-ui/react'
-import { ev, create } from '@open-wa/wa-automate'
+// import { ev, create } from '@open-wa/wa-automate'
+// "@open-wa/wa-automate": "^4.12.3",
 
 import { Footer } from '../../components/Dashboard/Common/Footer'
 import { Header } from '../../components/Dashboard/Common/Header'
@@ -8,10 +9,10 @@ import { App } from '../../components/Dashboard'
 export default function Dashboard(props) {
   return (
     <Flex
-      flexDirection="column"
-      justifyContent="space-between"
-      w="100vw"
-      h="100vh"
+      flexDirection='column'
+      justifyContent='space-between'
+      w='100vw'
+      h='100vh'
     >
       <Header />
       <App />
@@ -20,23 +21,23 @@ export default function Dashboard(props) {
   )
 }
 
-export async function getServerSideProps() {
-  function start(client) {
-    console.log(client)
-  }
-  create({
-    qrTimeout: 0,
-  }).then(start)
+// export async function getServerSideProps() {
+//   function start(client) {
+//     console.log(client)
+//   }
+//   create({
+//     qrTimeout: 0,
+//   }).then(start)
 
-  ev.on('qr.**', async (qrcode) => {
-    const imageBuffer = Buffer.from(
-      qrcode.replace('data:image/png;base64,', ''),
-      'base64',
-    )
-    fs.writeFileSync('qr.png', imageBuffer)
-  })
+//   ev.on('qr.**', async (qrcode) => {
+//     const imageBuffer = Buffer.from(
+//       qrcode.replace('data:image/png;base64,', ''),
+//       'base64'
+//     )
+//     fs.writeFileSync('qr.png', imageBuffer)
+//   })
 
-  let data = 'opa'
+//   let data = 'opa'
 
-  return { props: { data } }
-}
+//   return { props: { data } }
+// }
